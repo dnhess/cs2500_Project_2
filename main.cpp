2b787064f5cc53d9b67c340849c7debb36828708
Dillon Hess <dnhc59@mst.edu>
2017-04-24 20:34:26 -0500

    Added Static?

--- main.cpp
+++ main.cpp
@@ -11,16 +11,27 @@ using namespace std;
 
 
 int depth;
+ofstream fot;
+ofstream fdp;
+ofstream ipk;
+
 //Declaring Functions
 void vecerase(vector< list< pair<int, int> > > &adjL, int i, int s);
-bool bfs(vector< list< pair<int, int> > > adjL, int s, int d, int V);
-int fordFulkerson(vector< list< pair<int, int> > > adjL, int s, int d, int V);
+bool bfs(vector< list< pair<int, int> > > &adjL, int s, int d, int V);
+int fordFulkerson(vector< list< pair<int, int> > > adjL, int s, int d, int V, list<pair<int,int>> &to_delete, pair<int,int> &simple_delete, bool &firstrun);
+
+void routeone(vector<list<pair<int, int> > > adjL, int s, int d, int V,list<pair<int,int>> &to_delete, pair<int,int> & simple_delete);
 
 int main() {
     int vertices;
+    int flow;
     srand(time(NULL));
     int id, loc, countv = 0, counte = 0, source, dest;
+    list<pair<int,int>> to_delete;
+    pair<int,int> simple_delete;
     bool firstfind = true;
+
+    //Opening Output Files
 
     string line;
     ifstream fin;
@@ -105,42 +116,23 @@ int main() {
     }
     fin.close();
 
-//
-//    for (int i = 0; i < adjacencyList.size(); ++i) {
-//        printf("adjacencyList[%d] ", i);
-//
-//        list< pair<int, int> >::iterator itr = adjacencyList[i].begin();
-//
-//        while (itr != adjacencyList[i].end()) {
-//            printf(" -> %d(%d)", itr->first, itr->second);
-//            ++itr;
-//        }
-//        printf("\n");
-//    }
-
 
     do
     {
         source = rand() % vertices;
         dest = rand() % vertices;
-//        printf("Source: %d Dest: %d \n", source, dest);
-//        printf("True of false? %d \n", bfs(adjacencyList, source, dest, vertices));
-//        printf("Depth: %d \n", depth);
     }while(!(bfs(adjacencyList, source, dest, vertices) && depth >= 10));
-//    cout <<"source: "<<source<<endl;
-//    cout <<"dest: "<<dest<<endl;
-//        for (int i = 0; i < adjacencyList.size(); ++i) {
-//        printf("adjacencyList[%d] ", i);
-//
-//        list< pair<int, int> >::iterator itr = adjacencyList[i].begin();
-//
-//        while (itr != adjacencyList[i].end()) {
-//            printf(" -> %d(%d)", itr->first, itr->second);
-//            ++itr;
-//        }
-//        printf("\n");
-//    }
-    cout <<"Result: \n"<<fordFulkerson(adjacencyList, source, dest, vertices)<<endl;
+
+    //===========Change Source to weight 20===========
+    list< pair<int, int> >::iterator itr = adjacencyList[source].begin();
+    while (itr != adjacencyList[source].end()) {
+        itr->second = 20;
+        ++itr;
+    }
+    cout <<"Source: "<<source<<endl;
+    cout <<"Dest: "<<dest<<endl;
+    cout <<"Depth: "<<depth<<endl;
+    routeone(adjacencyList, source, dest, vertices, to_delete, simple_delete);
     return 0;
 
 }
@@ -160,11 +152,12 @@ void vecerase(vector< list< pair<int, int> > >& adjList, int i, int s)
 
 
 //Checks to see if the selected values can actually be reached
-bool bfs(vector<list<pair<int, int> > > adjL, int s, int d, int V) {
+bool bfs(vector<list<pair<int, int> > > &adjL, int s, int d, int V) {
 
 
     bool *visited = new bool[V];
     depth = 0;
+    bool notzero = true;
     for(int i = 0; i < V; i++)
         visited[i] = false;
 
@@ -178,134 +171,136 @@ bool bfs(vector<list<pair<int, int> > > adjL, int s, int d, int V) {
     while(!queue.empty())
     {
         s = queue.front();
-        //cout <<s<<" "<<endl;
         queue.pop_front();
 
 
         for(itr = adjL[s].begin(); itr != adjL[s].end(); itr++)
         {
-            if(!visited[itr->first] && itr->second >= 1)
+            if(itr->second == 0)
+                notzero = false;
+           else if(!visited[itr->first] && itr->second >= 1)
             {
+                notzero = true;
                 visited[itr->first] = true;
                 if(itr->first == d) {
-//                    queue.clear();
+                    itr->second = 20;
                     return true;
                 }
                 queue.push_back((*itr).first);
             }
         }
-        depth++;
+        if(notzero)
+            depth++;
     }
    delete[](visited);
 
     return false;
 }
 
-int fordFulkerson(vector<list<pair<int, int> > > adjL, int s, int d, int V) {
+int fordFulkerson(vector<list<pair<int, int> > > adjL, int s, int d, int V,list<pair<int,int>> &to_delete, pair<int,int> & simple_delete, bool &firstrun1) {
     bool firstrun = true;
+    int max_delete = 0;
     int path_flow;
     int foundd = 0;
     int perms = s;
+    int countforvec = 0;
     int tempflow;
     bool addedmax = true;
     vector< list< pair<int, int> > > rlist(adjL);
 
     int max_flow = 0;
-    cout <<"GET HERES: "<<endl;
     list< pair<int, int> >::iterator itr = rlist[s].begin();
     list<int> queue;
-    //list<int> tofollow;
     queue.push_back(s);
 
-    cout <<"NOW HERE"<<endl;
     while(bfs(rlist,perms,d,V))
     {
         if(firstrun)
             path_flow = INT_MAX;
-        cout <<"Does this thing cause issues? L "<<endl;
         s = queue.front();
-        cout <<"What is in front of the queue? "<<queue.front()<<endl;
-        cout <<"What about this? : "<<endl;
-        //This causes issues when trying to pop 0
         queue.pop_front();
-        cout <<"Get past the popping? "<<endl;
 
         for(itr = rlist[s].begin(); itr != rlist[s].end(); ++itr) {
-            cout <<"Gets here"<<endl;
             if (firstrun)
                 firstrun = false;
-            cout << "ITR Value: " << itr->first << endl;
-            cout << "Weight: " << itr->second << endl;
             queue.push_back((*itr).first);
             if (foundd != 1 && itr->second != 0)
             {
                 path_flow = min(path_flow, itr->second);
             }
-            cout <<"path flow value: "<<path_flow<<endl;
             if(foundd == 1)
             {
-                cout <<"Old weight: "<<itr->second<<endl;
                 tempflow = itr->second;
                 itr->second -= path_flow;
                 if(itr->second < 0)
                     itr->second = 0;
+                if(max_delete < itr->second && s != perms && itr->first != d)
+                {
+                    max_delete = itr->second;
+                    //Change to list
+                    if(firstrun1) {
+                        to_delete.push_back(make_pair(s, itr->first));
+                    }
+                    simple_delete.first = s;
+                    simple_delete.second = itr->first;
+                }
                 if(tempflow == path_flow)
-                    goto jump;
-                cout <<"New Weight: "<<itr->second<<endl;
+                    if(foundd == 1 && !addedmax) {
+                        max_flow += path_flow;
+                        addedmax = true;
+                    }
             }
-            cout<<"Step 1"<<endl;
             if(itr->second < 0)
                 itr->second = 0;
-            cout<<"Step 2"<<endl;
             if(itr->first == d) {
                 if(foundd == 0) {
                     foundd++;
                     addedmax = false;
-                    cout <<"Gets in here1"<<endl;
                     queue.clear();
                     queue.push_back(perms);
-                    cout <<"After mumbo jumbo"<<endl;
                     break;
                 }
                 else{
                     foundd = 0;
                     path_flow = INT_MAX;
                     addedmax = false;
-                    cout <<"LELLLELELEWFWEF: "<<queue.size()<<endl;
                     queue.clear();
                     queue.push_back(perms);
-                    cout <<"AFTER CAADFAD"<<endl;
                     break;
                 }
             }
         }
-jump:
-        cout<<"Step 3"<<endl;
-        if(foundd == 1 && !addedmax) {
-            max_flow += path_flow;
-            addedmax = true;
-        }
-        //cout <<"Still reachable?: "<<bfs(rlist,s,d,V)<<endl;
     }
-    cout <<"Does it get here?: "<<endl;
+    firstrun1 = false;
     queue.clear();
     return max_flow;
 }
 
-
-/*
- * 322 -> 323(10) -> 429(3)
- * 323 -> 336(6)
- * 336
- * 429 -> 430(20) -> 431(19)
- * 430  -> 535(19)
- * 431 -> 432(16)
- * 432 -> 433(1) -> 437(14)
- * 433 -> 435(8)
- * 435
- * 437 -> 670
- * 535 -> 536(19)
- * 536 -> 537(10)
- * 536 -> 538(14)
- * 538
- */
+void routeone(vector<list<pair<int, int> > > adjL, int s, int d, int V,list<pair<int,int>> &to_delete, pair<int,int> & simple_delete) {
+    int time = 100;
+    bool firstrun = true;
+    bool firstrun1 = true;
+    cout <<"Depth inside routeone: "<<depth<<endl;
+    int tempdepth = depth;
+    int flow = fordFulkerson(adjL, s, d, V, to_delete, simple_delete, firstrun);
+    fot.open("../flowovertime.csv");
+    fot<<"Time, Flow"<<endl;
+    fdp.open("../flowdisp.csv"); //Not sure what to output on this one
+    ipk.open("../impactK.csv");
+    ipk<<"Time, Number of Nodes"<<endl;
+    do {
+        fot << time << "," << flow << endl;
+        if(firstrun1) {
+            firstrun1 = false;
+            ipk << time << "," << tempdepth << endl;
+        }
+        else
+            ipk << time <<"," << depth<<endl;
+        vecerase(adjL, to_delete.front().second, to_delete.front().first);
+        if(!to_delete.empty())
+            to_delete.pop_front();
+        time--;
+        flow = fordFulkerson(adjL, s, d, V, to_delete, simple_delete, firstrun);
+        bfs(adjL, s, d, V);
+    }while(time != 0);
+}
